30cdde5a2f2d0132edcc293c773e7d4d827ebf5a
Gabriel A S B Lima <gabriel_araujo388@hotmail.com>
2022-05-25 21:40:19 -0300

    Adicionando o Projeto

---
+++ cadastro.aula/src/projeto/cadastro/aluno/util/ConnectionFactory.java
@@ -0,0 +1,52 @@
+package projeto.cadastro.aluno.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+import projeto.cadastro.aluno.util.*;
+
+public class ConnectionFactory {
+	
+	
+	
+	public static Connection getConnection() throws Exception{
+		try {
+			
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String url = "jdbc:mysql://localhost:3306/DBLeitor";
+			String login ="root";
+			String senha = "";
+			
+			
+			return DriverManager.getConnection(url,login,senha);
+			
+						
+		}
+		catch (Exception erro) {
+			
+			
+			throw new Exception (erro.getMessage());
+			
+		}
+	}
+		
+		public static void main(String arg[]) {
+			
+			try {
+				
+				Connection conn = ConnectionFactory.getConnection();
+				JOptionPane.showMessageDialog(null,"Foi!");
+				
+				System.out.print("Foi!!!!");
+				
+			}
+			catch (Exception erro) {
+				
+				erro.printStackTrace();
+			}			
+		}
+}
